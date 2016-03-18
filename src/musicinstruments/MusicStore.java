be1989f4c4e6a7b93002c6e6d7cd0f6622f19522
AndriiMykhalniuk <amykhalnyuk@gmail.com>
2016-03-18 18:42:19 +0200

    HW6 done

---
+++ src/musicinstruments/MusicStore.java
@@ -0,0 +1,35 @@
+package musicinstruments;
+
+import java.util.List;
+
+/**
+ * Created by amikhalnyuk on 18.03.2016.
+ */
+class MusicStore {
+    int gitar;
+    int piano;
+
+    public int getGitar() {
+        return gitar;
+    }
+
+    public void setGitar(int gitar) {
+        this.gitar = gitar;
+    }
+
+    public int getPiano() {
+        return piano;
+    }
+
+    public void setPiano(int piano) {
+        this.piano = piano;
+    }
+
+    @Override
+    public String toString() {
+        return "MusicStore{" +
+                "piano=" + piano +
+                ", gitar=" + gitar +
+                '}';
+    }
+}
